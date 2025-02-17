a1156b859d81e7f9c9c29ae8ce86bf80b4a12e53
Mikelraz <mikelrazsol@gmail.com>
2025-02-17 12:17:26 -0500

    Add final code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,11 +5,11 @@ def process_response(response):
     json_response = json.loads(response.text)
 
     output_dict = {
-        'anger': json_response['emotionPredictions'][0]['emotion']['anger']
-        'disgust': json_response['emotionPredictions'][0]['emotion']['disgust']
-        'fear': json_response['emotionPredictions'][0]['emotion']['fear']
-        'joy': json_response['emotionPredictions'][0]['emotion']['joy']
-        'sadness': json_response['emotionPredictions'][0]['emotion']['sadness']
+        'anger': json_response['emotionPredictions'][0]['emotion']['anger'],
+        'disgust': json_response['emotionPredictions'][0]['emotion']['disgust'],
+        'fear': json_response['emotionPredictions'][0]['emotion']['fear'],
+        'joy': json_response['emotionPredictions'][0]['emotion']['joy'],
+        'sadness': json_response['emotionPredictions'][0]['emotion']['sadness'],
     }
 
     max_score = 0
@@ -28,6 +28,16 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    output = process_response(response)
+    if response.status_code == 400:
+        output =  {        
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
+    else:
+        output = process_response(response)
 
     return output
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Flask web application for emotion detection.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,19 +9,28 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def emo_detection():
+    """
+    Analyze the given text for emotions and return the response.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     output = (
-        F"For the given statement, the system response is 'anger' : {response['anger']}, "
-        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} "
-        f"and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+        f"For the given statement, the system response is 'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, "
+        f"'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
     )
 
     return output
 
 @app.route("/")
 def render_index_page():
+    """
+    Render the index page of the application.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
